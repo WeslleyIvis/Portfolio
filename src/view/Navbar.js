7171da930be549f53f44d6a576f45e53448ea070
WeslleyIvis <weslley_reive@hotmail.com>
2023-08-31 04:54:31 -0300

    Delete comps

---
+++ src/view/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="navbar">
+        <div>IV</div>
+        <ul>
+          <li>About Me</li>
+          <li>Project</li>
+          <li>Contact me</li>
+        </ul>
+      </div>
+      {/* <div className="menu-mobile">
+        <div className="menu-hamburguer"></div>
+      </div>
+      
+      <div className="mobile-menu">
+        <ul>
+          <li>About Me</li>
+          <li>Project</li>
+          <li>Contact me</li>
+        </ul>
+      </div> */}
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/view/Skills.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Skills = () => {
+  const [data, setData] = React.useState(null);
+
+  const getData = (setState) => {
+    fetch('data.json', {
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((r) => r.json())
+      .then((json) => setState(json));
+  };
+
+  React.useEffect(() => {
+    getData(setData);
+  }, []);
+
+  return (
+    <section className="skills-content padding-150">
+      <div className="skills">
+        <div className="title-skills">
+          <h2>Skills</h2>
+          <span>I have experience with these techonologies</span>
+        </div>
+        {data !== null ? (
+          <div className="skills-items">
+            {data.skills.map((element) => {
+              return (
+                <div key={element.name}>
+                  {<h3>{element.name}</h3>}
+                  <ul style={{ paddingLeft: '10px' }}>
+                    {element.skill.map((element) => {
+                      return <li key={element}>{element}</li>;
+                    })}
+                  </ul>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
