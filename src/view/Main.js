4e04e5bae5830986bcb20895d77a7b58656e3dd9
WeslleyIvis <weslley_reive@hotmail.com>
2023-08-31 04:55:08 -0300

    Refactor main

---
+++ src/view/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Aboutme from '../components/Aboutme';
+import Projects from '../components/Projects';
+
+const Main = () => {
+    return (
+        <main>
+            <Aboutme />
+            <Projects />
+        </main>
+    );
+};
+
+export default Main;
