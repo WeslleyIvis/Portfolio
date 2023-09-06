97d5e85c41d5afff7269429f16ba90a0e5587f08
WeslleyIvis <weslley_reive@hotmail.com>
2023-09-05 23:16:56 -0300

    Coment selectd slide

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,10 +5,14 @@ const Projects = (projects) => {
     return (
         <section className="content-project">
             <h1 className="h1-sub-title">My Projects</h1>
-            <ul className="select-slider">
-                <li className="selected">All Projects</li>
-                <li>UI/UX Design</li>
-            </ul>
+            {/* <ul className="select-slider">
+                <li className="selected">
+                    <p>All Projects</p>
+                </li>
+                <li>
+                    <p>UI/UX Design</p>
+                </li>
+            </ul> */}
 
             {projects ? <Slider data={projects} /> : null}
         </section>
