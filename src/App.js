61c51d3c180bfddfb5285e993f2617b20420f1b9
WeslleyIvis <weslley_reive@hotmail.com>
2023-09-07 20:19:28 -0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import '../src/styles/rootStyles.css';
 import Header from '../src/view/Header.js';
+import Footer from './view/Footer';
 import Main from './view/Main';
 
 function App() {
-  return (
-    <div className="bg-color-body">
-      <Header></Header>
-      <Main />
-    </div>
-  );
+    return (
+        <div className="bg-color-body">
+            <Header></Header>
+            <Main />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
